81984c3d223b2ab67a37f93ac2eb1f3ad4dd98d3
F3RN4ND02 <fernando.ujap@gmail.com>
2022-10-04 04:27:56 -0400

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
       <ApolloProvider client={client}>
         <Repository/>
       </ApolloProvider>
-      {/*<Experience/>*/}
+      <Experience/>
       <Contact />
     </div>
   );
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,107 +1,122 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import Slide from '@mui/material/Slide';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { blue, red } from '@mui/material/colors';
-import DialogTitle from '@mui/material/DialogTitle';
-import {img} from '../../img/gs.png'
-import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
-import { experience } from "./Data"
+import {img} from '../../img/gs.png'
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
+ function Experience() {
+  const [expanded, setExpanded] = React.useState(false);
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
-function Experience() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
   };
 
   return (
-    <div>
-        <Container maxWidth="xl">
-        <Typography variant="h4" color="initial" className='title'>Work Experience</Typography>
-<Grid
-  container
-  spacing={6}
-  direction="row"
-  justify="center"
-  alignItems="center"
-  alignContent="center"
-  wrap="nowrap"
-  
->
-  
-{experience.map((data,key) => {
-                                return(
-                                    <Grid item>
 
-<Card sx={{ minWidth: 350 }}>
+    <Container maxWidth="xl">
+    <Typography variant="h4" color="initial" className='title'>Work Experience</Typography>
+    <Grid
+    container
+    spacing={6}
+    direction="row"
+    justify="center"
+    alignItems="center"
+    alignContent="center"
+    wrap="nowrap"
+
+    >
+      <Grid item>
+    <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        title={data.name}
-        subheader={data.time}
+        title="NA - Technical Support Engineer"
+        subheader="Jun 2021 - Present"
       />
       <CardMedia
         component="img"
-        height="180"
-        image= {data.img}
-        alt="gs"
+        height="194"
+        image="/static/images/cards/paella.jpg"
+        alt="WE"
       />
       <CardContent>
-        <Typography variant="body3" color="text.primary">
-        {data.position}
+        <Typography variant="body2" color="text.secondary">
+          Applying the knowledge about VoIP, SIP, RFC, Working with clients about API integrations from servers to endpoints.
         </Typography>
       </CardContent>
-      <CardActions>
-      <Typography variant="h6" color="initial" style={{marginLeft:'70px'}} onClick={data.onclick}>See more</Typography>
-        <AddCircleOutlineSharpIcon variant="outlined" onClick={data.onclick} fontSize='large'/>
-        <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={data.hclose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{data.position}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            {data.desc}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={data.hclose}>Awesome!</Button>
-
-        </DialogActions>
-      </Dialog>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
       </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+            aside for 10 minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+            large plate and set aside, leaving chicken and chorizo in the pan. Add
+            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+            stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don&apos;t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
     </Card>
-</Grid>
-)
-})}
-</Grid>          
+    </Grid>
+    </Grid>          
         </Container>
 
-    </div>
   );
 }
 export {Experience}
